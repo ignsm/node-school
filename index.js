310197299869097e794db7868b2e18ffb1fa08fa
Ignat Smirnov <ignsm@yandex.ru>
2017-08-11 03:36:25 +0300

    validate functions refactoring

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-// Маска ввода телефона
-var phoneNumber = new PhoneMask(document.getElementById("phoneMask"), {
-  pattern: "+7(___)___-__-__",
-  patternChar: "_",
-  allowedRegExp: /^\d$/
-});
-
+// Глобальный объект MyForm
 function MyForm(id) {
   this.form = document.getElementById(id);
-  this.button = this.form.querySelectorAll("#submitButton");
-  this.button[0].onclick = () => {
+  this.button = this.form.querySelectorAll("#submitButton")[0];
+  this.button.onclick = () => {
     this.submit();
   };
 }
-
-// При отправке формы должна срабатывать валидация полей по следующим правилам:
-// * ФИО: Ровно три слова.
-// * Email: Формат email-адреса, но только в доменах ya.ru, yandex.ru, yandex.ua, yandex.by, yandex.kz, yandex.com.
-// * Телефон: Номер телефона, который начинается на +7, и имеет формат +7(999)999-99-99. Кроме того, сумма всех цифр телефона не должна превышать 30. Например, для +7(111)222-33-11 сумма равняется 24, а для +7(222)444-55-66 сумма равняется 47.
 
 // В глобальной области видимости должен быть определен объект MyForm с методами
 // validate() => { isValid: Boolean, errorFields: String[] }
@@ -24,74 +13,93 @@ function MyForm(id) {
 // setData(Object) => undefined
 // submit() => undefined
 
-function throwError(el) {
-  if (el.classList) el.classList.add("error");
-  else el.className = "error";
-  return 0;
-}
-
 MyForm.prototype.validate = function() {
   // Обрабатываем инпуты
   let inputs = Array.from(this.form.querySelectorAll("input"));
   for (let i = 0; i < inputs.length; i++) {
     switch (inputs[i].getAttribute("name")) {
       case "fio": // Валидируем имя
-        let arrNames = inputs[i].value.split(" ").filter(el => {
-          return el.replace(/[^A-Za-zА-Яа-яё]/gim, "");
-        });
-        if (arrNames.length == 3) {
-          inputs[i].classList.remove("error");
-        } else {
-          throwError(inputs[i]);
-        }
+        this.nameValidation(inputs[i]);
         break;
       case "email": // Валидируем почту
-        let emails = [
-            "ya.ru",
-            "yandex.ru",
-            "yandex.ua",
-            "yandex.by",
-            "yandex.kz",
-            "yandex.com"
-          ],
-          regularEmailChecker = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (regularEmailChecker.test(inputs[i].value.replace(" ", ""))) {
-          let mailProvider = inputs[i].value.split("@");
-          let flag = 0;
-          for (let j = 0; j < emails.length; j++) {
-            if (emails[j] == mailProvider[1]) {
-              inputs[i].classList.remove("error");
-              break;
-            } else {
-              flag++;
-            }
-          }
-          if (flag == emails.length) throwError(inputs[i]);
-        } else {
-          throwError(inputs[i]);
-        }
+        this.emailValidation(inputs[i]);
         break;
-      case "phone":
-        // Валидируем телефон
-        let numArray = inputs[i].value.replace(/[^0-9]/gim, "").split(""),
-          sumNum = 0;
-        for (var k = 0; k < numArray.length; k++) {
-          sumNum += numArray[k];
-        }
-        if (numArray.length == 11) {
-          for (var k = 0; k < numArray.length; k++) {
-            sumNum += numArray[k];
-          }
-          if (sumNum < 30) {
-            throwError(inputs[i]);
-          }
-        } else {
-          throwError(inputs[i]);
-        }
+      case "phone": // Валидируем телефон
+        this.phoneValidate(inputs[i]);
         break;
       default:
         break;
     }
+  }
+};
+
+// Функция показа ошибки
+function throwError(el) {
+  if (el.classList) el.classList.add("error");
+  else el.className = "error";
+  return 0;
+}
+
+// Функция обработки ФИО
+MyForm.prototype.nameValidation = function(el) {
+  let arrNames = el.value.split(" ").filter(el => {
+    return el.replace(/[^A-Za-zА-Яа-яё]/gim, "");
+  });
+  if (arrNames.length == 3) {
+    el.classList.remove("error");
+  } else {
+    throwError(el);
+  }
+};
+
+// Функция обработки Email
+MyForm.prototype.emailValidation = function(el) {
+  let emails = [
+      "ya.ru",
+      "yandex.ru",
+      "yandex.ua",
+      "yandex.by",
+      "yandex.kz",
+      "yandex.com"
+    ],
+    regularEmailChecker = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regularEmailChecker.test(el.value.replace(" ", ""))) {
+    let mailProvider = el.value.split("@");
+    let flag = 0;
+    for (let j = 0; j < emails.length; j++) {
+      if (emails[j] == mailProvider[1]) {
+        el.classList.remove("error");
+        break;
+      } else {
+        flag++;
+      }
+    }
+    if (flag == emails.length) throwError(el);
+  } else {
+    throwError(el);
+  }
+};
+
+// Маска ввода телефона
+var phoneNumber = new PhoneMask(document.getElementById("phoneMask"), {
+  pattern: "+7(___)___-__-__",
+  patternChar: "_",
+  allowedRegExp: /^\d$/
+});
+
+// Валидируем номер телефона
+MyForm.prototype.phoneValidate = function(el) {
+  let numArray = el.value.replace(/[^0-9]/gim, "").split(""),
+    sumNum = 0;
+  if (numArray.length == 11) {
+    for (var k = 0; k < numArray.length; k++) {
+      sumNum += +numArray[k];
+    }
+    if (sumNum > 30) {
+      throwError(el);
+    }
+  } else {
+    throwError(el);
   }
 };
 
